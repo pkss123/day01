1a7e9788e8e746a046da21d0d96e0a315b4948d6
pkss123 <80875171+pkss123@users.noreply.github.com>
2021-05-17 21:50:35 +0900

    Add files via upload
    C언어 1일차

---
+++ day01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*
+	기본 입출력 함수 사용
+	C언어에서 기본적으로 제공하는 stdio.h 헤더 파일을 프로그램안에 포함
+*/
+int main() {
+	// printf("hello world");
+
+	// printf("내용 입력\n");
+	// printf("두줄 한번에 출력\n");
+
+	/*printf("즐거운\t하루\t보내세요\t~\n");
+	printf("이름\t: 박영민\n");
+	printf("나이\t: 23\n");
+	printf("사는곳\t: 북구\n");*/
+
+	//printf("12345\n");
+	//printf("12\b345\n");
+
+	//printf("1234567890\n");
+	//printf("12345\r67890\n");
+	//printf("아버지는 말하셨지 \"인생을 즐겨라 \"~");
+	printf("\ 20,000\n");
+	printf("\20,000\n");
+	printf("\\ 20,000\n");
+	printf("\\20,000\n");
+
+}
